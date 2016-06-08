5b6411e6f5e3e816f4c72c6c579d2262bdd12a92
Jordan Papaleo <papaleowebdev@gmail.com>
2016-06-08 10:36:16 -0700

    all data points rendered

--- app/index.js
+++ app/index.js
@@ -1,16 +1,23 @@
 'use strict';
 
 (function () {
-  const dataMapService = require('./dataMapService');
+  const reoMapService = require('./reoMapService');
   const processPdfService = require('./processPdfService');
   const Addendum = require('./Addendum');
 
   function processAddendum(file, cb) {
+    const addendum = new Addendum(file);
+
     processPdfService.load(file).then(
       (items) => {
-        const processedItems = dataMapService.load(items);
-        const addendum = new Addendum(processedItems);
-        cb(addendum.render());
+        addendum.processedPdf = items;
+
+        const results = reoMapService.load(items);
+
+        addendum.repairItems = results.repairItems;
+        addendum.asisItems = results.asisItems;
+
+        cb(addendum.data);
       },
       (err) => {
         console.error(`Error: ${err}`);
---
+++ app/reoMapService.js
@@ -0,0 +1,218 @@
+'use strict';
+
+const fs = require('fs');
+
+(function () {
+  const utils = require('./utils');
+  const THRESHOLD = 2; // Visual position difference allowed to match
+
+  function _positionSort(items) {
+    return items.sort((a, b) => {
+      return a.transform[5] - b.transform[5];
+    });
+  }
+
+  // There are a few cases in which we do not have the correct fields to format
+  function _shouldFormat(values) {
+    let hasValues = true;
+
+    if (!values[0] || !values[1]) {
+      hasValues = false;
+    } else if (values[0] === 'ESTIMATED COST' || values[1] === 'ESTIMATED COST') {
+      hasValues = false;
+    } else if (values[0] === 'REPAIR ITEM' || values[1] === 'REPAIR ITEM') {
+      hasValues = false;
+    }
+
+    return hasValues;
+  }
+
+  function _getRepairHotZone(item) {
+    const REPAIR_ITEMS = 12;
+    const ROW_HEIGHT = 9.2;
+    const minThreshold = item.transform[5] - REPAIR_ITEMS * ROW_HEIGHT;
+
+    return [minThreshold, item.transform[5]];
+  }
+
+  function _getAsIsZone(positions) {
+    const sorted = positions.sort();
+    const ROW_HEIGHT = 9.2;
+    return [sorted[0] - ROW_HEIGHT, sorted[sorted.length - 1]];
+  }
+
+  function _getHotZones(items) {
+    const hotZones = {};
+    const asis = [];
+
+    items.forEach((item) => {
+      const text = item.text.replace(' ', '').replace('-', '').toLowerCase();
+
+      if (text === 'repairitem') {
+        hotZones.repairItems = _getRepairHotZone(item);
+      } else if (text.indexOf('asis') !== -1 || text.indexOf('asrepaired') !== -1) {
+        asis.push(item.transform[5]);
+      }
+    });
+
+    hotZones.asisItems = _getAsIsZone(asis);
+
+    return hotZones;
+  }
+
+  const reoMapService = {
+    test(items) {
+      fs.writeFileSync('BLAR-PLOP.json', JSON.stringify(items, null, 2));
+    },
+    load(items) {
+      if (process.env.TEST_MODE) {
+        this.test(items);
+      }
+
+      const extractedData = {
+        repairItems: [],
+        asisItems: [],
+      };
+
+      this._items = items;
+      this.hotZones = _getHotZones(items);
+
+      const filteredAsIsItems = this.filterResults(items, this.hotZones.asisItems);
+      if (filteredAsIsItems && filteredAsIsItems.length > 0) {
+        const mappedAsIsValues = this.mapAsIsValues(filteredAsIsItems);
+        if (mappedAsIsValues && Object.keys(mappedAsIsValues).length > 0) {
+          extractedData.asisItems = this.formatAsIsValues(mappedAsIsValues);
+        }
+      }
+
+      const filteredRepairItems = this.filterResults(items, this.hotZones.repairItems);
+      if (filteredRepairItems && filteredRepairItems.length > 0) {
+        const matchedRepairValues = this.matchRepairValues(filteredRepairItems);
+        if (matchedRepairValues && matchedRepairValues.length > 0) {
+          extractedData.repairItems = this.formatRepairResults(matchedRepairValues);
+        }
+      }
+
+      return extractedData;
+    },
+    filterResults(items, dimensions) {
+      // filter data from the parsed pdf
+      if (dimensions) {
+        const min = dimensions[0];
+        const max = dimensions[1];
+
+        return items.filter((item) => {
+          const position = item.transform[5];
+
+          // Items with a Y transform within the hotzone
+          let isValid = (position >= min && position <= max);
+
+          // The following are junk values
+          if (item.text === '$' || item.text.indexOf('. . .') !== -1) {
+            isValid = false;
+          }
+
+          return isValid;
+        });
+      }
+    },
+    mapAsIsValues(items) {
+      const asisHash = {};
+      const sortedItems = _positionSort(items);
+
+      // Find the y transform of fields we need
+      sortedItems.forEach((item) => {
+        const key = ~~item.transform[5];
+        const parsedFloatVal = utils.tryParseFloat(item.text);
+
+        if (item.text.indexOf('AS-') === 1) {
+          if (!asisHash[key]) {
+            asisHash[key] = {};
+          }
+
+          asisHash[key].item = item.text;
+        }
+
+        if (parsedFloatVal) {
+          if (!asisHash[key]) {
+            asisHash[key] = {};
+          }
+
+          if (parsedFloatVal >= 120) {
+            asisHash[key].price = parsedFloatVal;
+          } else {
+            asisHash[key].days = parsedFloatVal;
+          }
+        }
+      });
+
+      return asisHash;
+    },
+    matchRepairValues(items) {
+      const matchedValues = [];
+      const sortedItems = _positionSort(items);
+
+      let previousPosition;
+      let tempVar = [];
+      for (let i = 0, j = sortedItems.length; i < j; i++) {
+        const item = sortedItems[i];
+        tempVar.push(item.text);
+
+        if (i === 0) {
+          previousPosition = item.transform[5];
+        } else {
+          if (Math.abs(previousPosition - item.transform[5]) <= THRESHOLD) {
+            if (tempVar.length === 2) {
+              matchedValues.push(tempVar);
+              tempVar = [];
+            }
+          }
+
+          previousPosition = item.transform[5];
+        }
+      }
+
+      return matchedValues;
+    },
+    formatRepairResults(matchedValues) {
+      // the values come back in a non predictable formatRepairResults
+      // this sorts the data pairs by their ability to parse into a number
+      const formattedResults = [];
+
+      for (let i = 0, j = matchedValues.length; i < j; i++) {
+        const values = matchedValues[i];
+
+        if (!_shouldFormat(values)) {
+          continue;
+        }
+
+        const parsedValue = utils.tryParseFloat(values[0]);
+
+        if (parsedValue) {
+          formattedResults.push({
+            item: values[1],
+            price: parsedValue,
+          });
+        } else {
+          formattedResults.push({
+            item: values[0],
+            price: utils.tryParseFloat(values[1]),
+          });
+        }
+      }
+
+      return formattedResults;
+    },
+    formatAsIsValues(items) {
+      const formattedResults = [];
+
+      Object.keys(items).forEach((key) => {
+        formattedResults.push(items[key]);
+      });
+
+      return formattedResults;
+    },
+  };
+
+  module.exports = reoMapService;
+}());
