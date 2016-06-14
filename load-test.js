6126ef3fce67a895e0d3ab41a7d00f369acc17f8
Jordan Papaleo <papaleowebdev@gmail.com>
2016-06-13 21:45:18 -0700

    working on testing

---
+++ load-test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+// Under Construction
+(function () {
+  fs.readdir('../test-data', (err, names) => {
+    names.forEach((name) => {
+      if (name.indexOf('.pdf') !== -1) {
+        readFile(name);
+      }
+    });
+  });
+
+  function readFile(file) {
+    fs.readFile(`../test-data/${file}`, (err, data) => {
+      if (err) throw err;
+
+      const END_POINT = 'http://SAAS-TR-L-001.local:8080/v1/pdf';
+      const options = {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/pdf'
+        },
+        body: data,
+        mode: 'cors'
+      };
+
+      fetch(END_POINT, options)
+        .then((res) => {
+          console.log('Here');
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    });
+  }
+}());
